112805169e984aaa307731505fb4f601b89c1daa
Lisa Mignot <lisammignot@gmail.com>
2025-03-13 16:17:20 -0300

    style: estilzando o projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import AdicionarContato from './containers/AdicionarContato'
+import BarraDeBusca from './containers/BarraDeBusca'
+import Cabecalho from './containers/Cabecalho'
+import ListaDeContatos from './containers/ListaDeContatos'
 import EstiloGlobal, { Container } from './styles'
 
 function App() {
-  return <h1>oie</h1>
+  return (
+    <>
+      <EstiloGlobal />
+      <Container>
+        <Cabecalho contador={1} />
+        <BarraDeBusca />
+        <AdicionarContato />
+        <ListaDeContatos />
+      </Container>
+    </>
+  )
 }
 
 export default App
